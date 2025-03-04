c7afd000e6bf14c16c2c149a4545bdf4b519d82b
Jose Gabriel Jiménez <74217610+josegabjimenez@users.noreply.github.com>
2025-03-03 21:56:58 -0500

    feat: integrate "emergencies reception page" with the backend using SSE (#4)
    * feat: create emergencies dashboard and emergency details
    
    * feat: integrate reception emergencies page with the backend

---
+++ api/sseClient.ts
@@ -0,0 +1,25 @@
+import {getCookie} from '@/utils/cookies';
+import {EventSourcePolyfill} from 'event-source-polyfill';
+
+export function createEventSource(url: string): EventSourcePolyfill {
+	const token = getCookie('authToken');
+
+	const eventSource = new EventSourcePolyfill(url, {
+		headers: {
+			Authorization: token ? `Bearer ${token}` : '',
+		},
+	});
+
+	eventSource.onmessage = (event) => {
+		const data = JSON.parse(event.data);
+		console.log('New message:', data.data);
+	};
+
+	eventSource.onerror = (error) => {
+		console.error('SSE error:', error);
+		// Optionally close connection on error:
+		// eventSource.close();
+	};
+
+	return eventSource;
+}
