6eefbf9dde60bd518e73308cf284ef3329dae031
Jose Gabriel Jiménez <74217610+josegabjimenez@users.noreply.github.com>
2025-03-09 16:37:12 -0500

    hotfix: add no available emergencies message (#6)

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -12,7 +12,7 @@ export default function Dashboard() {
 
 	console.log({data, isConnected, error});
 
-	if (!data) {
+	if (data === null) {
 		return (
 			<main className="min-h-screen bg-white p-4">
 				{/* Header */}
@@ -23,6 +23,22 @@ export default function Dashboard() {
 					<h1 className="text-3xl font-bold text-gray-900 mb-8 text-center">Emergencias a verificar</h1>
 					<p className="text-gray-600">Cargando emergencias...</p>
 					{/* <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-customRed"></div> */}
+				</div>
+			</main>
+		);
+	}
+
+	if (data === undefined) {
+		return (
+			<main className="min-h-screen bg-white p-4">
+				{/* Header */}
+				<SettingsMenu />
+
+				{/* Main Content */}
+				<div className="mt-12 px-4 flex flex-col items-center w-full">
+					<h1 className="text-3xl font-bold text-gray-900 mb-8 text-center">Emergencias a verificar</h1>
+					<p className="text-gray-600">No hay emergencias activas.</p>
+					{/* <div className="animate-pulse rounded-full h-32 w-32 border-t-2 border-b-2 border-customRed"></div> */}
 				</div>
 			</main>
 		);
